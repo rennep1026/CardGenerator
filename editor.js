ca6e975d7eb9e05b5036631a92820d3f7dc09031
Taylor Penner <rennep1026@gmail.com>
2015-09-16 16:08:38 -0700

    added ability to choose icons, added attributes, added description text area, added "add to print page".

--- editor.js
+++ editor.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     $(":input").bind("input change", function(){
-        var elemId = $(this).attr('id')
+        var elemId = $(this).attr('id');
         switch (elemId) {
             case "cardTitleIn":
                 $(".card-progress .card-title").text($(this).val());
@@ -39,11 +39,42 @@ $(document).ready(function(){
                 var abScore = $(this).val() || 10;
                 $(".card-progress .card-stats-cha p").text(abScore+" ("+abilityMod[abScore]+")");
                 break;
+            case "cardDesc":
+                var html = $('<p>').append($.parseHTML($(this).val()));
+                $(".card-progress .card-desc .card-text").empty().append(html);
+                break;
+            case "cardAttr":
+                if($(this).prop("checked") === true){
+                    $('.card-progress .card-inner').append($(attrArea).clone());
+                } else {
+                    $('.card-progress .card-bottom').remove();
+                }
+                break;
+            case "cardAttrText":
+                $(".card-progress .card-bottom p").text($(this).val());
+                break;
         }
     });
 
+    $('#cardAdd').click(function(){
+        var num = parseInt($("#cardCount").val() || 1);
+        console.log(num);
+        for(var i=0; i<num; i++){
+            $('.card-container').append($('.card-progress .card').clone());
+        }
+    });
+
+    var attrArea = $.parseHTML(
+        '<div class="card-bottom">'+
+            '<svg class="card-hr-style" width="100" height="1" viewBox="0 0 100 1">'+
+                '<polyline points="0,0 100,0.5 0,1" fill="black"></polyline>'+
+            '</svg>'+
+            '<p></p>'+
+        '</div>'
+    );
+
     var statsBlock = $.parseHTML(
-        '<div class="card-stats card-text">'+
+        '<div class="card-stats">'+
             '<div class="card-stats-str"><span>Str</span><p>10 (0)</p></div>'+
             '<div class="card-stats-dex"><span>Dex</span><p>10 (0)</p></div>'+
             '<div class="card-stats-con"><span>Con</span><p>10 (0)</p></div>'+
@@ -57,7 +88,32 @@ $(document).ready(function(){
                         '+1','+1','+2','+2','+3','+3','+4','+4','+5','+5','+6','+6',
                         '+7','+7','+8','+8','+9','+9','+10','+10'];
 
-    $(".card-stats").bind("change", function(){
+    var svgFiles = [];
 
+
+
+    $( "#svgViewer" ).dialog({
+        autoOpen: false,
+        width: 780,
+        height: 600
+    });
+    $( "#svgOpener" ).click(function() {
+        $.get("svg.txt", null, function(data){
+            svgFiles = data.split('\n');
+            console.log(svgFiles);
+            for(var i=0; i<svgFiles.length-1; i++){
+                var svgImg = $('<span class="card-icon">')
+                    .append($('<img class="lazy '+svgFiles[i]+'" data-original="svg/'+svgFiles[i]+'">'));
+                $('#svgViewer').append(svgImg);
+            }
+            $("img.lazy").lazyload({
+                container: $('#svgViewer'),
+                threshold: 200
+            });
+            $("#svgViewer .card-icon img").click(function(){
+                $('.card-progress .card-icon').empty().append($(this).clone());
+            });
+        });
+        $( "#svgViewer" ).dialog( "open" );
     });
 });
